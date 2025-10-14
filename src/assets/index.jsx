e2c66b84e24db62661d7935e9f4258aa959b8859
Finneyd16 <lavisco16@gmail.com>
2025-10-14 13:07:51 +0100

    fix

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -9,9 +9,9 @@ import P8 from "./P8.png";
 import P9 from "./P9.png";
 import P10 from "./P10.png";
 import P11 from "./P11.png";
-import P12 from "./p12.png";
-import P13 from "./p13.png";
-import P14 from "./p14.png";
+import P12 from "./P12.png";
+import P13 from "./P13.png";
+import P14 from "./P14.png";
 import P15 from "./P15.png";
 
 export { P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15 };
